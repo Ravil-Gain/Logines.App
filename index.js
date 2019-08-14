75ebf58c36dc9689fb9907d2f89715445f81eeb5
Ravil <ravil.gainutdinov@myjar.com>
2019-08-14 17:18:01 +0300

    model

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB_Connect,
+    { useNewUrlParser: true },
+    () => console.log('db connected!')
+);
 //import routes
 const authRoute = require('./routes/auth');
+
+app.use(express.json());
 
 //Routes Middlewares
 app.use('/api/user', authRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) =>{
+router.post('/register', async (req, res) =>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+
+        const saveUser = await user.save();
+        res.send(saveUser);
+    }catch(error){
+        res.status(400).send(error);
+    }
     res.send('register');
 });
 
